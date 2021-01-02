a264dcf106e7fa117f8ad7a8ef9e13f2c40a117f
raihan <ryihan55@gmail.com>
2021-01-03 04:26:23 +0600

    Add New Function Ip()
    Ip Address Traking .
    >>getenv() is used to get the value of an environment variable in PHP.
    Ip Address Traking .
    >>$_SERVER is an array that contains server variables created by the web server.

--- Class/PHP_Class/index.php
+++ Class/PHP_Class/index.php
@@ -167,9 +167,48 @@ class _
 				}else{
 					return $_SERVER['HTTP_HOST'];
 				}
-
-				
 			}
+	}
+	function Ip($a){
+		/*
+		*getenv()
+		*$_SERVER
+		*/
+		if($a=="nv"){
+			$ipaddress = '';
+		    if (getenv('HTTP_CLIENT_IP'))
+		        $ipaddress = getenv('HTTP_CLIENT_IP');
+		    else if(getenv('HTTP_X_FORWARDED_FOR'))
+		        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+		    else if(getenv('HTTP_X_FORWARDED'))
+		        $ipaddress = getenv('HTTP_X_FORWARDED');
+		    else if(getenv('HTTP_FORWARDED_FOR'))
+		        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+		    else if(getenv('HTTP_FORWARDED'))
+		       $ipaddress = getenv('HTTP_FORWARDED');
+		    else if(getenv('REMOTE_ADDR'))
+		        $ipaddress = getenv('REMOTE_ADDR');
+		    else
+		        $ipaddress = 'UNKNOWN';
+		    return $ipaddress;
+		}else{
+			$ipaddress = '';
+		    if (isset($_SERVER['HTTP_CLIENT_IP']))
+		        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+		    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+		        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+		        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+		    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+		        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+		    else if(isset($_SERVER['HTTP_FORWARDED']))
+		        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+		    else if(isset($_SERVER['REMOTE_ADDR']))
+		        $ipaddress = $_SERVER['REMOTE_ADDR'];
+		    else
+		        $ipaddress = 'UNKNOWN';
+		    return $ipaddress;
+		}
 	}
 
 }
