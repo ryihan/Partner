1f74c8d47a4d1d1b6271445061b618f42cbf4ec0
raihan <ryihan55@gmail.com>
2021-01-05 01:18:59 +0600

    Earning Report
    Earning Report  [Dashboard]

--- Dist/Dashboard/JSON/Data_PHP/earning.php
+++ Dist/Dashboard/JSON/Data_PHP/earning.php
@@ -4,34 +4,42 @@ require_once("../../../../Class/PHP_Class/index.php");
 $_ = new _();
 $_->DB_Con(1);
 //initlize Class
-$Ref = $User['user_name'];//AND `Now_Time` = ''
-$Query 	="SELECT * FROM `balance_history` WHERE `Ref`='$Ref'";	
+$Ref = $User['user_name'];
+//Today Earning Report
+$Query 	="SELECT * FROM `balance_history` WHERE DATE(Now_Time) = CURDATE() AND `Ref`='$Ref'";	
 $Count = $_->Get_Data_M($Query,"1");
 $Data = json_decode($Count,true);
 $Today =0;
-foreach($Data as $item) {
-    $Today =  $Today+$item['Add_Balance_Amount'];
    
-}
 
+	if(!$Data==0){
+		foreach($Data as $item) {
+		    $Today =  $Today+$item['Add_Balance_Amount'];
+		}
+	}
+//This Month Earning Report
 $Month = 0;
-
-$Query 	="SELECT * FROM `balance_history` WHERE `Ref`='$Ref'";	
+$Query 	="SELECT * FROM `balance_history` WHERE `Ref`='$Ref' AND MONTH(Now_Time) = MONTH(NOW()) AND YEAR(Now_Time) = YEAR(NOW())";	
 $Count = $_->Get_Data_M($Query,"1");
 $Data = json_decode($Count,true);
-foreach($Data as $item) {
     $Month =  $Month+$item['Add_Balance_Amount'];
-   
 }
-
+	if(!$Data==0){
+		foreach($Data as $item) {
+    		$Month =  $Month+$item['Add_Balance_Amount'];  
+		}
+	}
+//Alltime Earning Report
 $All   = 0;
 $Query 	="SELECT * FROM `balance_history` WHERE `Ref`='$Ref'";	
 $Count = $_->Get_Data_M($Query,"1");
 $Data = json_decode($Count,true);
-foreach($Data as $item) {
     $All =  $All+$item['Add_Balance_Amount'];
-   
-}//OK
+	if(!$Data==0){
+		foreach($Data as $item) {
+    		$All =  $All+$item['Add_Balance_Amount']; 
+		}
+	}
 //FeedBack
 echo '
 	{ 
