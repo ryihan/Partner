1a631c3cb1f2826047e0596f15111f1e4408ae04
raihan <ryihan55@gmail.com>
2021-01-05 01:04:21 +0600

    Sale Report Update
    Sale Report Update [Dashbord]

--- Dist/Dashboard/JSON/Data_PHP/sell.php
+++ Dist/Dashboard/JSON/Data_PHP/sell.php
@@ -1,16 +1,17 @@
 <?php 
-$Today = 500;
-$Month = 0;
-$All   = 0;
-
-
-
-
-
-
-
-
-
+//Include Class
+require_once("../../../../Class/PHP_Class/index.php");
+$_ = new _();
+$user_name = $User['user_name'];
+//Today Sale Report 
+$Query = "SELECT * FROM `order_history` WHERE DATE(Order_Date) = CURDATE() AND `ref`='$user_name'";
+$Today = $_->Data_Count($Query,2);
+//This Month Sale Report
+$Query = "SELECT * FROM `order_history` WHERE  `ref`='$user_name' AND MONTH(Order_Date) = MONTH(NOW()) AND YEAR(Order_Date) = YEAR(NOW())";
+$Month = $_->Data_Count($Query,2);
+//Alltime Sale Report
+$Query = "SELECT * FROM `order_history` WHERE `ref`='$user_name'";
+$All = $_->Data_Count($Query,2);
 //FeedBack
 echo '
 	{ 
